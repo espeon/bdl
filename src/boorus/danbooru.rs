315539027fe4be502ac2e32aaf7095ca7e6cf561
Emma Lo <emma@iscute.dev>
2020-10-03 04:27:48 -0500

    better error messages

--- src/boorus/danbooru.rs
+++ src/boorus/danbooru.rs
@@ -2,21 +2,19 @@ use crate::BooruInfo;
 use crate::Result;
 
 pub async fn danbooru(info: BooruInfo) -> anyhow::Result<String> {
-    let image_result =
-        reqwest::get(&format!("https://{}/posts/{}.json", info.host, info.id))
-            .await?
-            .json::<Result>()
-            .await?;
+    let image_result = reqwest::get(&format!("https://{}/posts/{}.json", info.host, info.id))
+        .await?
+        .json::<Result>()
+        .await?;
     Ok(image_result.file_url)
 }
 
-pub async fn danbooru_parser(url: url::Url, host:String) -> anyhow::Result<BooruInfo> {
+pub async fn danbooru_parser(url: url::Url, host: String) -> anyhow::Result<BooruInfo> {
     let pairs = url.path();
-    let id = pairs.split("/").collect::<Vec<&str>>()[2].parse::<i64>().unwrap();
-    let ret = BooruInfo {
-        host: host,
-        id: id,
-    };
+    let id = pairs.split("/").collect::<Vec<&str>>()[2]
+        .parse::<i64>()
+        .unwrap();
+    let ret = BooruInfo { host: host, id: id };
 
     Ok(ret)
 }
--- src/boorus/yandere.rs
+++ src/boorus/yandere.rs
@@ -1,23 +1,28 @@
-use crate::BooruInfo;
 use crate::ArrayResult;
+use crate::BooruInfo;
+use anyhow::anyhow;
 
 pub async fn yandere(info: BooruInfo) -> anyhow::Result<String> {
-    let image_result =
-        reqwest::get(&format!("https://{}/post.json?tags=id:{}", info.host, info.id))
-            .await?
-            .json::<ArrayResult>()
-            .await.unwrap();
-    dbg!(&image_result);
+    let image_result = reqwest::get(&format!(
+        "https://{}/post.json?tags=id:{}",
+        info.host, info.id
+    ))
+    .await?
+    .json::<ArrayResult>()
+    .await
+    .unwrap();
+    if image_result.is_empty() {
+        return Err(anyhow!("Post not found."));
+    }
     Ok(image_result[0].file_url.clone())
 }
 
-pub async fn yandere_parser(url: url::Url, host:String) -> anyhow::Result<BooruInfo> {
+pub async fn yandere_parser(url: url::Url, host: String) -> anyhow::Result<BooruInfo> {
     let pairs = url.path();
-    let id = pairs.split("/").collect::<Vec<&str>>()[3].parse::<i64>().unwrap();
-    let ret = BooruInfo {
-        host: host,
-        id: id,
-    };
+    let id = pairs.split("/").collect::<Vec<&str>>()[3]
+        .parse::<i64>()
+        .unwrap();
+    let ret = BooruInfo { host: host, id: id };
 
     Ok(ret)
 }
